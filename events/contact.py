4294f3fe08af7c509ec633b824e2effd5892efb2
ray <kazauta1989@gmail.com>
2019-10-16 18:07:37 +0800

    ButtonsTemplate

--- events/contact.py
+++ events/contact.py
@@ -10,11 +10,14 @@ def contact_event(event):
     buttons_template_message = TemplateSendMessage(
         alt_text='Buttons template',
         template=ButtonsTemplate(
+            # 圖片、標題、說明
             thumbnail_image_url='https://i.imgur.com/izzpeRb.jpg',
             title='聯絡我們',
             text='請選擇',
             actions=[
                 URIAction(
+                    # label 按鈕名稱
+                    # uri 可以填網址也可以填電話
                     label='打給我們',
                     uri='tel:+1234567799'
                 )
